90851dfd6d6ebadd9a4395ca039ca6de4baca9db
bhattmilind <bhattmilind@gmail.com>
2017-02-11 14:42:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/ui/sample1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sample1.html'));
+});
+
+app.get('/ui/sample2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sample2.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sample3.html'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
